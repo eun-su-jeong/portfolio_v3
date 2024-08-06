e2e0d83772164c59ead4ab5a6ed70c2d60f07e6b
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2024-08-06 14:18:55 +0900

    fix: 화면 안나옴

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
         <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
+  </React.StrictMode>
 );
